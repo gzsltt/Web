6ea9c91374b7fb5355d943d0a9590c36f3718d56
gzsltt <violet_li@outlook.com>
2018-11-26 16:42:45 +0800

    exercise

---
+++ layout.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link href="CSS/Mainbody.css" rel="stylesheet" type="text/css" />
+<link href="CSS/800px.css" rel="stylesheet" type="text/css" media="screen and (max-width: 800px)"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Layout</title>
+<style type="text/css">
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+</style>
+</head>
+
+<body>
+<div id="Container">
+<div id="Header"></div>
+<div id="Menu"></div>
+<div id="SideBar"></div>
+<div id="MainBody"></div>
+<div id="Footer"></div>
+
+</div>
+</body>
+</html>
