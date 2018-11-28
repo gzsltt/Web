5ad49335a5257410462e7ccbb526e69004632965
gzsltt <violet_li@outlook.com>
2018-11-28 11:20:02 +0800

    update

--- layout.php
+++ layout.php
@@ -5,6 +5,7 @@
 <link href="CSS/800px.css" rel="stylesheet" type="text/css" media="screen and (max-width: 800px)"/>
 <link href="CSS/Menu.css" rel="stylesheet" type="text/css"/>
 <link href="CSS/Header.css" rel="stylesheet" type="text/css"/>
+<link href="CSS/SideBar.css" rel="stylesheet" type="text/css"/>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Layout</title>
 <style type="text/css">
@@ -24,14 +25,29 @@ body {
 </h1>
 </div>
 <div id="Menu">
+<ul>
 <li><a href="#">Home</a></li>
 <li><a href="#">Services</a></li>
 <li><a href="#">About</a></li>
 <li><a href="#">Contact</a></li>
 </ul>
 </div>
-<div id="SideBar"></div>
-<div id="MainBody"></div>
+<div id="SideBar">
+<ul>
+<li><a href="#">Resume</a></li>
+<li><a href="#">My Blog</a></li>
+<li><a href="#">Music</a></li>
+<li><a href="#">Movies</a></li>
+</ul>
+</div>
+<div id="MainBody">
+<img src="back.jpg" alt="ltt" height="402"/>
+<div id="Introduction">
+<p>
+Stay Hungry
+</p>
+</div>
+</div>
 <div id="Footer"></div>
 </div>
 </body>
